bf7575ced971aba5fea788bcd1e60a9698383f8b
Diya Kithani <121928589+diyakithani@users.noreply.github.com>
2024-06-06 21:45:10 -0400

    Contains duplicate optimal approach

---
+++ Java/Day3_containsduplicate/optimal.java
@@ -0,0 +1,12 @@
+class optimal{
+    public boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> set = new HashSet<>();
+         for(int i = 0; i<nums.length;i++){
+            set.add(nums[i]);
+        }
+        if(set.size()<nums.length){
+            return true;
+        }
+        return false;
+        } 
+}
